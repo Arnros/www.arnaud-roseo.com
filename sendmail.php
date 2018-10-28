3c40fd05f31d4702945924bd25a76bdfc59392a4
Arnaud Roseo <arnaud.roseo@ouest-france.fr>
2018-10-28 10:02:27 +0100

    change sendmail settings

--- sendmail.php
+++ sendmail.php
@@ -1,22 +1,21 @@
-<?php
-    //we need to get our variables first
-    
-    $email_to =   'contact@arnaud-roseo.com'; //the address to which the email will be sent
-    $name     =   $_POST['name'];  
+<?php    
+    $to =   'contact@arnaud-roseo.com'; //the address to which the email will be sent
     $email    =   $_POST['email'];
     $subject  =   $_POST['subject'];
     $message  =   $_POST['message'];
     
-    /*the $header variable is for the additional headers in the mail function,
-     we are asigning 2 values, first one is FROM and the second one is REPLY-TO.
-     That way when we want to reply the email gmail(or yahoo or hotmail...) will know 
-     who are we replying to. */
-    $headers  = "From: contact@arnaud-roseo.com\r\n";
-    $headers .= "Reply-To: $email\r\n";
-    
-    if(mail($email_to, $subject, $message, $headers)){
-        echo 'sent'; // we are sending this text to the ajax request telling it that the mail is sent..      
+    $headers = '';
+    $headers .= 'MIME-Version: 1.0'."\n";
+    $headers .= 'content-type: text/html; charset=iso-8859-1'."\n";
+    $headers .= "From: {$to}\n";
+    $headers .= "Reply-To: {$email}\n";
+    $headers .= 'CC: arnaud.roseo@protonmail.com'."\n";
+    $headers .= 'X-Mailer: PHP v'.phpversion();
+
+
+    if(mail($to,$subject,$message,$headers)){
+        echo 'sent';  
     }else{
-        echo 'failed';// ... or this one to tell it that it wasn't sent    
+        echo 'failed';
     }
 ?>
