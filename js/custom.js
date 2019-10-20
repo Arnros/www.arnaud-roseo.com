48eca37fcf4f4095d7ef5c55bee5d256232b7781
Arnros <arnaud.roseo@gmail.com>
2019-10-20 10:04:15 +0200

    restore code

--- js/custom.js
+++ js/custom.js
@@ -198,91 +198,299 @@ jQuery(function ($) {
 
 
 	/* ========================================================================= */
-	/*	On scroll fade/bounce fffect
+	/*   Contact Form Validating
 	/* ========================================================================= */
 
-	wow = new WOW({
-		animateClass: 'animated',
-		offset: 120
+
+	$('#contact-submit').click(function (e) {
+
+		//stop the form from being submitted
+		e.preventDefault();
+
+		/* declare the variables, var error is the variable that we use on the end
+		to determine if there was an error or not */
+		var error = false;
+		var name = $('#name').val();
+		var email = $('#email').val();
+		var subject = $('#subject').val();
+		var message = $('#message').val();
+
+		/* in the next section we do the checking by using VARIABLE.length
+		where VARIABLE is the variable we are checking (like name, email),
+		length is a JavaScript function to get the number of characters.
+		And as you can see if the num of characters is 0 we set the error
+		variable to true and show the name_error div with the fadeIn effect. 
+		if it's not 0 then we fadeOut the div( that's if the div is shown and
+		the error is fixed it fadesOut. 
+		
+		The only difference from these checks is the email checking, we have
+		email.indexOf('@') which checks if there is @ in the email input field.
+		This JavaScript function will return -1 if no occurrence have been found.*/
+		if (name.length == 0) {
+			var error = true;
+			$('#name').css("border-color", "#D8000C");
+		} else {
+			$('#name').css("border-color", "#666");
+		}
+		if (email.length == 0 || email.indexOf('@') == '-1') {
+			var error = true;
+			$('#email').css("border-color", "#D8000C");
+		} else {
+			$('#email').css("border-color", "#666");
+		}
+		if (subject.length == 0) {
+			var error = true;
+			$('#subject').css("border-color", "#D8000C");
+		} else {
+			$('#subject').css("border-color", "#666");
+		}
+		if (message.length == 0) {
+			var error = true;
+			$('#message').css("border-color", "#D8000C");
+		} else {
+			$('#message').css("border-color", "#666");
+		}
+
+		//now when the validation is done we check if the error variable is false (no errors)
+		if (error == false) {
+			//disable the submit button to avoid spamming
+			//and change the button text to Sending...
+			$('#contact-submit').attr({
+				'disabled': 'false',
+				'value': 'Sending...'
+			});
+
+			/* using the jquery's post(ajax) function and a lifesaver
+			function serialize() which gets all the data from the form
+			we submit it to send_email.php */
+			$.post("sendmail.php", $("#contact-form").serialize(), function (result) {
+				//and after the ajax request ends we check the text returned
+				if (result == 'sent') {
+					//if the mail is sent remove the submit paragraph
+					$('#cf-submit').remove();
+					//and show the mail success div with fadeIn
+					$('#mail-success').fadeIn(500);
+				} else {
+					//show the mail failed div
+					$('#mail-fail').fadeIn(500);
+					//re enable the submit button by removing attribute disabled and change the text back to Send The Message
+					$('#contact-submit').removeAttr('disabled').attr('value', 'Send The Message');
+				}
+			});
+		}
 	});
-	wow.init();
 
-
-	/* ========================================================================= */
-	/*	Home page Slider
-	/* ========================================================================= */
-
-	$(function () {
-
-		var Page = (function () {
-
-			var $navArrows = $('#nav-arrows'),
-				$nav = $('#nav-dots > span'),
-				slitslider = $('#slitSlider').slitslider({
-
-					speed: 1600,
-
-					onBeforeChange: function (slide, pos) {
-
-						$nav.removeClass('nav-dot-current');
-						$nav.eq(pos).addClass('nav-dot-current');
-
-					}
-				}),
-
-				init = function () {
-					initEvents();
-				},
-				initEvents = function () {
-					// add navigation events
-					$navArrows.children(':last').on('click', function () {
-						slitslider.next();
-						return false;
-					});
-
-					$navArrows.children(':first').on('click', function () {
-						slitslider.previous();
-						return false;
-					});
-
-					$nav.each(function (i) {
-						$(this).on('click', function (event) {
-							var $dot = $(this);
-							if (!slitslider.isActive()) {
-								$nav.removeClass('nav-dot-current');
-								$dot.addClass('nav-dot-current');
-							}
-
-							slitslider.jump(i + 1);
-							return false;
-
-						});
-					});
-				};
-			return { init: init };
-
-		})();
-
-		Page.init();
-
-	});
 
 
 	/* ========================================================================= */
-	/*	Parallax Sections
-	/* ========================================================================= */
+	/*	Google Map Customization
+	/* =========================================================================  */
+
+	function initialize() {
+
+		var myLatLng = new google.maps.LatLng(22.333851, 91.812256);
+
+		var roadAtlasStyles = [{
+			"featureType": "landscape",
+			"elementType": "geometry.fill",
+			"stylers": [{
+				"color": "#2F3238"
+			}]
+		}, {
+			"elementType": "labels.text.fill",
+			"stylers": [{
+				"color": "#FFFFFF"
+			}]
+		}, {
+			"elementType": "labels.text.stroke",
+			"stylers": [{
+				"visibility": "off"
+			}]
+		}, {
+			"featureType": "road",
+			"elementType": "geometry.fill",
+			"stylers": [{
+				"color": "#50525f"
+			}]
+		}, {
+			"featureType": "road",
+			"elementType": "geometry.stroke",
+			"stylers": [{
+				"visibility": "on"
+			}, {
+				"color": "#808080"
+			}]
+		}, {
+			"featureType": "poi",
+			"elementType": "labels",
+			"stylers": [{
+				"visibility": "off"
+			}]
+		}, {
+			"featureType": "transit",
+			"elementType": "labels.icon",
+			"stylers": [{
+				"visibility": "off"
+			}]
+		}, {
+			"featureType": "poi",
+			"elementType": "geometry",
+			"stylers": [{
+				"color": "#808080"
+			}]
+		}, {
+			"featureType": "water",
+			"elementType": "geometry.fill",
+			"stylers": [{
+				"color": "#3071a7"
+			}, {
+				"saturation": -65
+			}]
+		}, {
+			"featureType": "road",
+			"elementType": "labels.icon",
+			"stylers": [{
+				"visibility": "off"
+			}]
+		}, {
+			"featureType": "landscape",
+			"elementType": "geometry.stroke",
+			"stylers": [{
+				"color": "#bbbbbb"
+			}]
+		}];
+
+		var mapOptions = {
+			zoom: 14,
+			center: myLatLng,
+			disableDefaultUI: true,
+			scrollwheel: false,
+			navigationControl: false,
+			mapTypeControl: false,
+			scaleControl: false,
+			draggable: false,
+			mapTypeControlOptions: {
+				mapTypeIds: [google.maps.MapTypeId.ROADMAP, 'roadatlas']
+			}
+		};
+
+		var map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
 
 
-	"use strict";
+		var marker = new google.maps.Marker({
+			position: myLatLng,
+			map: map,
+			icon: 'img/location-icon.png',
+			title: '',
+		});
 
-	function parallaxInit() {
-		$('#counter').parallax("50%", 0.3);
-		$('#team-skills').parallax("50%", 0.3);
-		$('#twitter-feed').parallax("50%", 0.3);
-		$('#testimonial').parallax("50%", 0.3);
+
+		google.maps.event.addListener(marker, 'click', function () {
+			infowindow.open(map, marker);
+		});
+
+		var styledMapOptions = {
+			name: 'US Road Atlas'
+		};
+
+		var usRoadMapType = new google.maps.StyledMapType(
+			roadAtlasStyles, styledMapOptions);
+
+		map.mapTypes.set('roadatlas', usRoadMapType);
+		map.setMapTypeId('roadatlas');
 	}
 
-	$(window).bind("load", function () {
-		parallaxInit()
-	});
+	/*	google.maps.event.addDomListener(window, "load", initialize); */
+
+
+});
+
+/* ========================================================================= */
+/*	On scroll fade/bounce fffect
+/* ========================================================================= */
+
+wow = new WOW({
+	animateClass: 'animated',
+	offset: 120
+});
+wow.init();
+
+
+/* ========================================================================= */
+/*	Home page Slider
+/* ========================================================================= */
+
+$(function () {
+
+	var Page = (function () {
+
+		var $navArrows = $('#nav-arrows'),
+			$nav = $('#nav-dots > span'),
+			slitslider = $('#slitSlider').slitslider({
+
+				speed: 1600,
+
+				onBeforeChange: function (slide, pos) {
+
+					$nav.removeClass('nav-dot-current');
+					$nav.eq(pos).addClass('nav-dot-current');
+
+				}
+			}),
+
+			init = function () {
+				initEvents();
+			},
+			initEvents = function () {
+				// add navigation events
+				$navArrows.children(':last').on('click', function () {
+					slitslider.next();
+					return false;
+				});
+
+				$navArrows.children(':first').on('click', function () {
+					slitslider.previous();
+					return false;
+				});
+
+				$nav.each(function (i) {
+					$(this).on('click', function (event) {
+						var $dot = $(this);
+						if (!slitslider.isActive()) {
+							$nav.removeClass('nav-dot-current');
+							$dot.addClass('nav-dot-current');
+						}
+
+						slitslider.jump(i + 1);
+						return false;
+
+					});
+				});
+			};
+		return { init: init };
+
+	})();
+
+	Page.init();
+
+});
+
+
+/* ========================================================================= */
+/*	Parallax Sections
+/* ========================================================================= */
+
+
+"use strict";
+
+function parallaxInit() {
+	$('#counter').parallax("50%", 0.3);
+	$('#team-skills').parallax("50%", 0.3);
+	$('#twitter-feed').parallax("50%", 0.3);
+	$('#testimonial').parallax("50%", 0.3);
+}
+
+$(window).bind("load", function () {
+	parallaxInit()
+});
 
