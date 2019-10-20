e9792ea2f9c0a2655d97fb1c9698fae0a9469b74
Arnros <arnaud.roseo@gmail.com>
2019-10-20 09:59:19 +0200

    fix color scrollbar

--- js/custom.js
+++ js/custom.js
@@ -1,11 +1,12 @@
-                                
 
-jQuery(function ($) { "use strict";
-	
+
+jQuery(function ($) {
+	"use strict";
+
 	/* ========================================================================= */
 	/*	Page Preloader
 	/* ========================================================================= */
-	
+
 	window.onload = function () {
 		document.getElementById('loading-mask').style.display = 'none';
 	}
@@ -13,10 +14,10 @@ jQuery(function ($) { "use strict";
 	/* =========================================================================== */
 	/*	FitVids js
 	/* =========================================================================== */
-	
+
 	$(".media-wrapper").fitVids();
 
-	
+
 	/* ========================================================================= */
 	/*	Nice Scroll - Custom Scrollbar
 	/* ========================================================================= */
@@ -25,7 +26,7 @@ jQuery(function ($) { "use strict";
 		cursorborderradius: 0,
 		cursorwidth: "8px",
 		cursorfixedheight: 150,
-		cursorcolor: "#6CB670",
+		cursorcolor: "#f48d17",
 		zindex: 9999,
 		cursorborder: 0,
 	});
@@ -34,18 +35,18 @@ jQuery(function ($) { "use strict";
 	/* ========================================================================= */
 	/*	Scroll Up / Back to top
 	/* ========================================================================= */
-	
-	$(window).scroll(function() {
+
+	$(window).scroll(function () {
 		if ($(window).scrollTop() > 400) {
 			$("#scrollUp").fadeIn(200);
 		} else {
 			$("#scrollUp").fadeOut(200);
 		}
 	});
-	
-	$('#scrollUp').click(function() {
+
+	$('#scrollUp').click(function () {
 		$('html, body').stop().animate({
-			scrollTop : 0
+			scrollTop: 0
 		}, 1500, 'easeInOutExpo');
 	});
 
@@ -53,26 +54,26 @@ jQuery(function ($) { "use strict";
 	/* ========================================================================= */
 	/*	Post image slider
 	/* ========================================================================= */
-	
+
 	$("#post-thumb, #gallery-post").owlCarousel({
 
-		navigation : true,
-		pagination : false,
-		slideSpeed : 800,
-		autoHeight : true,
-		paginationSpeed : 800,
-		singleItem:true,
-		navigationText : ["<i class='fa fa-angle-left fa-2x'></i>","<i class='fa fa-angle-right fa-2x'></i>"]
+		navigation: true,
+		pagination: false,
+		slideSpeed: 800,
+		autoHeight: true,
+		paginationSpeed: 800,
+		singleItem: true,
+		navigationText: ["<i class='fa fa-angle-left fa-2x'></i>", "<i class='fa fa-angle-right fa-2x'></i>"]
 
 	});
-	
+
 	$("#features").owlCarousel({
-		navigation : false,
-		pagination : true,
-		slideSpeed : 800,
-		singleItem : true,
-		transitionStyle : "fadeUp",
-		paginationSpeed : 800,
+		navigation: false,
+		pagination: true,
+		slideSpeed: 800,
+		singleItem: true,
+		transitionStyle: "fadeUp",
+		paginationSpeed: 800,
 	});
 
 
@@ -82,9 +83,9 @@ jQuery(function ($) { "use strict";
 
 
 	$("#navigation").sticky({
-		topSpacing : 0
+		topSpacing: 0
 	});
-	
+
 	$('#nav').onePageNav({
 		currentClass: 'current',
 		changeHash: false,
@@ -93,51 +94,52 @@ jQuery(function ($) { "use strict";
 		filter: '',
 		easing: 'easeInOutExpo'
 	});
-	
+
 
 
 	/* ========================================================================= */
 	/*	Fix Slider Height
-	/* ========================================================================= */	
+	/* ========================================================================= */
 
 	var slideHeight = $(window).height();
-	
-	$('#slitSlider, .sl-slider, .sl-content-wrapper').css('height',slideHeight);
 
-	$(window).resize(function(){'use strict',
-		$('#slitSlider, .sl-slider, .sl-content-wrapper').css('height',slideHeight);
+	$('#slitSlider, .sl-slider, .sl-content-wrapper').css('height', slideHeight);
+
+	$(window).resize(function () {
+		'use strict',
+		$('#slitSlider, .sl-slider, .sl-content-wrapper').css('height', slideHeight);
 	});
-	
-	
+
+
 
 	/* ========================================================================= */
 	/*	Timer count
 	/* ========================================================================= */
 
-	(function() {
+	(function () {
 		var count = {
-		  initialized : false,
-		  initialize : function() {
-			if (this.initialized)
-			  return;
-			this.initialized = true;
-			this.build();
-		  },
-		  build : function() {
-			this.animations();
-		  },
-		  animations : function() {
-			// Count To
-			$(".counters-item [data-to]").each(function() {
-			  var $this = $(this);
-			  $this.appear(function() {
-				$this.countTo({});
-			  }, {
-				accX : 0,
-				accY : -150
-			  });
-			});
-		  },
+			initialized: false,
+			initialize: function () {
+				if (this.initialized)
+					return;
+				this.initialized = true;
+				this.build();
+			},
+			build: function () {
+				this.animations();
+			},
+			animations: function () {
+				// Count To
+				$(".counters-item [data-to]").each(function () {
+					var $this = $(this);
+					$this.appear(function () {
+						$this.countTo({});
+					}, {
+						accX: 0,
+						accY: -150
+					});
+				});
+			},
 		};
 		count.initialize();
 	})();
@@ -159,18 +161,18 @@ jQuery(function ($) { "use strict";
 			}
 		})
 	});
-	
-	
+
+
 	/* ========================================================================= */
 	/*	Twitter Feed
 	/* ========================================================================= */
-	
-/*	$(".tweet").twittie({
-        dateFormat: "%b. %d, %Y",
-        template: "{{tweet}}",
-        count: 1
-    });
-*/
+
+	/*	$(".tweet").twittie({
+			dateFormat: "%b. %d, %Y",
+			template: "{{tweet}}",
+			count: 1
+		});
+	*/
 
 	/* ========================================================================= */
 	/*	Portfolio Filtering Hook
@@ -179,19 +181,19 @@ jQuery(function ($) { "use strict";
 	$('#og-grid').mixItUp(); // Portfolio filter
 
 	Grid.init(); //Portfolio Grid Expand
-	
-	
+
+
 	/* ========================================================================= */
 	/*	Testimonial Carousel
 	/* =========================================================================  */
- 
+
 	//Init the carousel
 	$("#testimonials").owlCarousel({
 		slideSpeed: 500,
 		paginationSpeed: 500,
 		singleItem: true,
-		pagination : true,
-		transitionStyle : "backSlide"
+		pagination: true,
+		transitionStyle: "backSlide"
 	});
 
 
@@ -398,7 +400,7 @@ jQuery(function ($) { "use strict";
 		map.setMapTypeId('roadatlas');
 	}
 
-/*	google.maps.event.addDomListener(window, "load", initialize); */
+	/*	google.maps.event.addDomListener(window, "load", initialize); */
 
 
 });
@@ -407,65 +409,65 @@ jQuery(function ($) { "use strict";
 /*	On scroll fade/bounce fffect
 /* ========================================================================= */
 
-	wow = new WOW({
-		animateClass: 'animated',
-		offset: 120
-	});
-	wow.init();
-	
+wow = new WOW({
+	animateClass: 'animated',
+	offset: 120
+});
+wow.init();
+
 
 /* ========================================================================= */
 /*	Home page Slider
 /* ========================================================================= */
 
-$(function() {
+$(function () {
 
-	var Page = (function() {
+	var Page = (function () {
 
-		var $navArrows = $( '#nav-arrows' ),
-			$nav = $( '#nav-dots > span' ),
-			slitslider = $( '#slitSlider' ).slitslider( {
-			
-			    speed : 1600,
-			
-				onBeforeChange : function( slide, pos ) {
+		var $navArrows = $('#nav-arrows'),
+			$nav = $('#nav-dots > span'),
+			slitslider = $('#slitSlider').slitslider({
 
-					$nav.removeClass( 'nav-dot-current' );
-					$nav.eq( pos ).addClass( 'nav-dot-current' );
+				speed: 1600,
+
+				onBeforeChange: function (slide, pos) {
+
+					$nav.removeClass('nav-dot-current');
+					$nav.eq(pos).addClass('nav-dot-current');
 
 				}
-			} ),
+			}),
 
-			init = function() {
-				initEvents();				
+			init = function () {
+				initEvents();
 			},
-			initEvents = function() {
+			initEvents = function () {
 				// add navigation events
-				$navArrows.children( ':last' ).on( 'click', function() {
+				$navArrows.children(':last').on('click', function () {
 					slitslider.next();
 					return false;
-				} );
+				});
 
-				$navArrows.children( ':first' ).on( 'click', function() {					
+				$navArrows.children(':first').on('click', function () {
 					slitslider.previous();
 					return false;
 				});
 
-				$nav.each( function( i ) {				
-					$( this ).on( 'click', function( event ) {						
-						var $dot = $( this );						
-						if( !slitslider.isActive() ) {
-							$nav.removeClass( 'nav-dot-current' );
-							$dot.addClass( 'nav-dot-current' );						
+				$nav.each(function (i) {
+					$(this).on('click', function (event) {
+						var $dot = $(this);
+						if (!slitslider.isActive()) {
+							$nav.removeClass('nav-dot-current');
+							$dot.addClass('nav-dot-current');
 						}
-						
-						slitslider.jump( i + 1 );
+
+						slitslider.jump(i + 1);
 						return false;
-					
-					});					
+
+					});
 				});
 			};
-			return { init : init };
+		return { init: init };
 
 	})();
 
@@ -489,6 +491,6 @@ function parallaxInit() {
 }
 
 $(window).bind("load", function () {
-    parallaxInit()
+	parallaxInit()
 });
-                            
+
